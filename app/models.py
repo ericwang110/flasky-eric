f242128680e299ee1d261fc7030d31736c613531
ericwang110 <1060339962@qq.com>
2015-04-07 15:57:08 +0800

    8-8
    加载用户的回调函数

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
-
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
+from . import db, login_manager
 from flask.ext.login import UserMixin
 
 
@@ -35,3 +34,8 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+@login_manager.user_loader()
+def load_user(user_id):
+    return User.query.get(int(user_id))
