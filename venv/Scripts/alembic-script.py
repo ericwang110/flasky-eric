6b930285c8e635eaa1ffbe1bb5944130ceccd580
ericwang110 <1060339962@qq.com>
2015-04-07 12:20:27 +0800

    hello-world
    Hello World!

---
+++ venv/Scripts/alembic-script.py
@@ -0,0 +1,10 @@
+#!C:\WQG-PC\PycharmProjects\flasky-Eric\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'alembic==0.7.5.post2','console_scripts','alembic'
+__requires__ = 'alembic==0.7.5.post2'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('alembic==0.7.5.post2', 'console_scripts', 'alembic')()
+    )
---
+++ venv/Scripts/mako-render-script.py
@@ -0,0 +1,10 @@
+#!C:\WQG-PC\PycharmProjects\flasky-Eric\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Mako==1.0.1','console_scripts','mako-render'
+__requires__ = 'Mako==1.0.1'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('Mako==1.0.1', 'console_scripts', 'mako-render')()
+    )
