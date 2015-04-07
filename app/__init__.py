861bdd32d75b5291081b168633e002ad520ec905
ericwang110 <1060339962@qq.com>
2015-04-07 15:46:24 +0800

    示例8-7
    初始化Flask-Login

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,15 @@ from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.sqlAlchemy import SQLAlchemy
 from config import config
+from flask.ext.login import LoginManager
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -20,6 +24,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
