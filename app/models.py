962d744bb7dfad317f4210832400ed87620d6242
ericwang110 <1060339962@qq.com>
2015-04-07 15:33:46 +0800

    示例8-5
    修改User模型，支持用户登录

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask.ext.login import UserMixin
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -12,9 +14,10 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
     password_hash = db.Column(db.String(128))
