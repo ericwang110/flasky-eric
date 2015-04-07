b7cfff9b481a035a32777a240d104802644103ad
ericwang110 <1060339962@qq.com>
2015-04-07 15:13:07 +0800

    示:8-3/8-4/8-5
    8-3：创建蓝本(auth/__init__.py)；8-4：蓝本中的路由和视图函数(auth/views.py)；8-5：附加蓝本(app/__init__.py)

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,31 @@
-__author__ = 'WQG-PC'
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.moment import Moment
+from flask.ext.sqlAlchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    return app
+
